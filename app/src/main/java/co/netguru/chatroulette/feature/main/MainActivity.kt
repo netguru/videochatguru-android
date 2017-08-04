3dab6fea3e54ccec21fc16669c4018d068970744
Rafal Naniewicz <rafal.naniewicz@netguru.pl>
2017-08-04 15:41:47 +0200

    Remove unused code

--- app/src/main/java/co/netguru/chatroulette/feature/main/MainActivity.kt
+++ app/src/main/java/co/netguru/chatroulette/feature/main/MainActivity.kt
@@ -1,31 +1,21 @@
 package co.netguru.chatroulette.feature.main
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import co.netguru.chatroulette.R
 import co.netguru.chatroulette.app.App
 import co.netguru.chatroulette.feature.base.BaseMvpActivity
 import co.netguru.chatroulette.feature.main.video.VideoFragment
-import co.netguru.chatroulette.feature.main.video.VideoFragmentOld
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : BaseMvpActivity<MainView, MainPresenter>(), MainView {
 
     val videoFragment = VideoFragment.newInstance()
-    //val videoFragment = VideoFragmentOld.newInstance()
-
 
     override fun retrievePresenter() = App.getApplicationComponent(this).mainComponent().mainPresenter()
 
     override fun getLayoutId() = R.layout.activity_main
-
-    fun Context.startMainActivity() {
-        val intent = Intent(this, MainActivity::class.java)
-        this.startActivity(intent)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/co/netguru/chatroulette/feature/main/video/SdpSetObserver.kt
@@ -0,0 +1,15 @@
+package co.netguru.chatroulette.feature.main.video
+
+import org.webrtc.SdpObserver
+import org.webrtc.SessionDescription
+
+interface SdpSetObserver : SdpObserver {
+
+    override fun onCreateSuccess(sessionDescription: SessionDescription) {
+        throw IllegalStateException("onCreateSuccess called in set listener")
+    }
+
+    override fun onCreateFailure(error: String) {
+        throw IllegalStateException("onCreateFailure called in set listener")
+    }
+}
