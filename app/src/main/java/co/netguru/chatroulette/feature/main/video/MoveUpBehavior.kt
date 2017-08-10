e3bdac64ecf831313a185eb532a2228a642efc9b
Rafal Naniewicz <rafal.naniewicz@netguru.pl>
2017-08-10 13:24:51 +0200

    Remove layoutDependsOn

--- app/src/main/java/co/netguru/chatroulette/feature/main/video/MoveUpBehavior.kt
+++ app/src/main/java/co/netguru/chatroulette/feature/main/video/MoveUpBehavior.kt
@@ -1,16 +1,11 @@
 package co.netguru.chatroulette.feature.main.video
 
 import android.support.design.widget.CoordinatorLayout
-import android.support.design.widget.Snackbar
 import android.view.Gravity
 import android.view.View
 
 
 class MoveUpBehavior : CoordinatorLayout.Behavior<View>() {
-
-    override fun layoutDependsOn(parent: CoordinatorLayout, child: View, dependency: View): Boolean {
-        return dependency is Snackbar.SnackbarLayout
-    }
 
     override fun onAttachedToLayoutParams(lp: CoordinatorLayout.LayoutParams) {
         if (lp.dodgeInsetEdges == Gravity.NO_GRAVITY) {
